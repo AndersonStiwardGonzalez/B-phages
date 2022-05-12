b17970e920689fdc248f71a03481788117720484
AndersonStiwardGonzalez <105405956+AndersonStiwardGonzalez@users.noreply.github.com>
2022-05-11 22:29:20 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+#Local URL: http://localhost:8501 Network URL: http://192.168.1.2:8501
+import streamlit as st
+st.set_page_config(page_title="B-phages", page_icon=":tada:", layout="wide")
+st.subheader("¡Hola! Somos B-Phages :wave:")
+st.title("Somos una empresa comprometida con tu salud")
+st.write("Queremos darte el mejor de los tratamientos contra las infecciones por bacterias multiresistentes")
+st.write("Somos los mejores, somos una retrochimba")
+st.write("[Aprende más de como ser cool --->](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+st.audio("y2mate.com - Bad Bunny  Tu No Metes Cabra Video Oficial.mp3")
