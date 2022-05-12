86b9e43c32c4e7323ed365a954b3953cd1f187a6
AndersonStiwardGonzalez <105405956+AndersonStiwardGonzalez@users.noreply.github.com>
2022-05-12 11:15:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Local URL: http://localhost:8501 Network URL: http://192.168.1.2:8501
 import streamlit as st
-st.set_page_config(page_title="B-", page_icon=":tada:", layout="wide")
-st.subheader("¡Hola! Somos una chimba :wave:")
+st.set_page_config(page_title="B-phages", page_icon=":dna:,:petri_dish:,:test_tube:,:microscope:	", layout="wide")
+st.subheader("¡Hola! Somos uB-phages :wave:")
 st.title("Somos una empresa comprometida con tu salud")
 st.write("Queremos darte el mejor de los tratamientos contra las infecciones por bacterias multiresistentes")
 st.write("Somos los mejores, somos una retrochimba")
