5d91c58e60dfaedf8fe1b5753d4a392ead24b249
AndersonStiwardGonzalez <105405956+AndersonStiwardGonzalez@users.noreply.github.com>
2022-05-12 20:55:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -89,9 +89,9 @@ with st.container():
   st.write("---")
   st.title("¡Contáctanos!")
   i_column,w_column,c_column,y_column = st.columns(4)
-   with i_column:
-     st.write("Instagram")
-     with w_column:
-       st.write("WhatsApp")
+  with i_column:
+    st.write("Instagram")
+    with w_column:
+      st.write("WhatsApp")
  
  
