a27bacc2a424bc74b078fb38d1d0dbc2d5518a53
tmolinab <105464621+tmolinab@users.noreply.github.com>
2022-05-12 19:29:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 #pagina: https://share.streamlit.io/andersonstiwardgonzalez/aaaaaaaaaaaaaaaaaaaa/main/app.py
 import streamlit as st
 
-st.title("[¡Hola! Somos B-phages :wave:](Font="Cooper Black)")
+st.title("¡Hola! Somos B-phages :wave:")
 
 st.subheader("Somos una empresa comprometida con tu salud")
 st.write("Queremos darte el mejor de los tratamientos contra las infecciones por bacterias multiresistentes")
