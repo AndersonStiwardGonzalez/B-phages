8ef3c02674ac982ee30a522dce103277c49a276f
AndersonStiwardGonzalez <105405956+AndersonStiwardGonzalez@users.noreply.github.com>
2022-05-17 16:25:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,11 @@ with st.container():
         with w_column:
             st.write("WhatsApp")
             st.image("_MG_1613.jpg")
+            [theme]
+primaryColor = "#E694FF"
+backgroundColor = "#00172B"
+secondaryBackgroundColor = "#0083B8"
+textColor = "#C6CDD4"
+font = "sans-serif"
             
 
