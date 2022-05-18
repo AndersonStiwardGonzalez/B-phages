90acfcfec10dc50a793fa467a2eba725254e9ff8
AndersonStiwardGonzalez <105405956+AndersonStiwardGonzalez@users.noreply.github.com>
2022-05-17 23:47:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ with st.container():
                  st.image("youtube.png")
                  with c_column:
                      st.write("G-mail")
-                     st.image("gmail.png",caption=("G-MAIL)., width(3))
+                     st.image("gmail.png",caption=("G-MAIL), width(3))
 
